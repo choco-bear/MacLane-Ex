1896aa95d0f4e85b440a6f83909a7442aa2bae76
choco-bear <kdy030529@gmail.com>
2026-01-03 09:28:00 +0900

    Skip the section 6

---
+++ Ch_I/Section6.v
@@ -0,0 +1,4 @@
+(** The section 6 is about universe -- in the Grothendieck sense -- so it has no
+  * meaning to formalize the exercises in the section 6. Hence, I decided to ignore
+  * section 6 in the project.
+  *)
