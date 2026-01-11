4c02f6b5af6bd316aaeaf1b8779b3a7342ff8bb6
choco-bear <kdy030529@gmail.com>
2026-01-11 23:26:23 +0900

    Solved Ex II.4.2

--- Ch_II/Section4.v
+++ Ch_II/Section4.v
@@ -1,6 +1,7 @@
 Require Import Category.Lib.
 From Category.Theory Require Import
   Category
+  Isomorphism
   Functor
   Natural.
 From Category.Construction Require Product Fun.
@@ -10,7 +11,21 @@ Generalizable All Variables.
 
 (* TODO : Ex1. *)
 
-(* TODO : Ex2. *)
+Module Ex2. Section Ex2.
+  Import Fun Product Discrete Cat.
+  Context (B : Category) (X : Type).
+  Context `{FIN : FinType X}.
+
+  Lemma ex2 : Fun[Cat[X],B] ≅[Cat] ∏ (λ x : X, B).
+    isomorphism.
+    - apply ProductFunctor. i. construct; [exact (X0 i)|exact (f i)|..]; cat.
+    - s. srapply (@FromAFunctor _ _ (λ F, _)).
+      { exact (Functor_from_function F). }
+      by construct; try natural_transform; ss; subst.
+    - cat.
+    - by cat; try isomorphism; try natural_transform; ss; subst.
+  Qed.
+End Ex2. End Ex2.
 
 (* TODO : Ex3. *)
 
