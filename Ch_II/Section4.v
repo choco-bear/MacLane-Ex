11fd41c8441335a02fcf8f9aaa98368a8bb1930b
choco-bear <kdy030529@gmail.com>
2026-01-12 12:46:53 +0900

    Solved Ex II.4.7

--- Ch_II/Section4.v
+++ Ch_II/Section4.v
@@ -6,7 +6,7 @@ From Category.Theory Require Import
   Natural.
 From Category.Facts Require Group.Automorphism.
 From Category.Construction Require Product Fun.
-From Category.Instance Require PreOrder Discrete Cat Fin Grp.
+From Category.Instance Require PreOrder Discrete Cat Fin Grp Two.
 
 Generalizable All Variables.
 
@@ -90,6 +90,94 @@ End Ex5. End Ex5.
 
 (* TODO : Ex6. *)
 
-(* TODO : Ex7. *)
+Module Ex7. Section Ex7.
+  Import Two Fun.
+  Context (B : Category) (C : Category).
+
+  Section H_determines.
+    Context (H : C ⟶ Fun[2,B]).
+
+    Program Definition S : C ⟶ B :=
+      {| fobj := λ c, H c TwoA ; fmap := λ _ _ f, fmap[H] f TwoA |}.
+    Next Obligation. now proper; sufficient (fmap[H] x0 ≡ fmap[H] y0); rewrites. Qed.
+    Next Obligation. now sufficient (fmap[H] id[x] ≡ id); rewrite fmap_id. Qed.
+    Next Obligation. now sufficient (fmap[H] (f ∘ g) ≡ fmap[H] f ∘ fmap[H] g); normalize. Qed.
+
+    Program Definition T : C ⟶ B :=
+      {| fobj := λ c, H c TwoB ; fmap := λ x y f, fmap[H] f TwoB |}.
+    Next Obligation. now proper; sufficient (fmap[H] x0 ≡ fmap[H] y0); rewrites. Qed.
+    Next Obligation. now sufficient (fmap[H] id[x] ≡ id); rewrite fmap_id. Qed.
+    Next Obligation. now sufficient (fmap[H] (f ∘ g) ≡ fmap[H] f ∘ fmap[H] g); normalize. Qed.
+
+    Program Definition τ : S ⟹ T :=
+      {| component := λ c, fmap[H c] TwoF |}.
+    Next Obligation. now normalize. Qed.
+  End H_determines.
+
+  Section Triple_determines.
+    Context (S T : C ⟶ B) (τ : S ⟹ T).
+
+    Program Definition H_fobj (c : C) : Fun[2,B] :=
+      {|  fobj := λ x : 2, match x with
+                           | TwoA => S c
+                           | TwoB => T c
+                           end
+        ; fmap := λ x y (f : x ~{2}~> y), match f with
+                                          | TwoIdA => id[S c]
+                                          | TwoIdB => id[T c]
+                                          | TwoF   => τ c
+                                          end
+      |}.
+    Next Obligation. now destruct x. Qed.
+    Next Obligation.
+      pose proof (TwoHom_inv f). pose proof (TwoHom_inv g).
+      unfold TwoHom_inv_t, Two._2_obligation_1 in *.
+      by destruct x, y, z; ss; subst.
+    Qed.
+
+    Program Definition H : C ⟶ Fun[2,B] :=
+      {|  fobj := H_fobj
+        ; fmap := λ x y f,
+            {|  component := λ o : 2, match o return (∀ x y f, H_fobj x o ~> H_fobj y o) with
+                                      | TwoA => λ _ _, fmap[S]
+                                      | TwoB => λ _ _, fmap[T]
+                                      end x y f
+              ; naturality := _
+            |}
+      |}.
+    Next Obligation.
+      pose proof (TwoHom_inv f0). unfold TwoHom_inv_t in X.
+      now destruct x0, y0; subst; ss; normalize.
+    Qed.
+    Next Obligation. now proper; destruct x1; rewrites. Qed.
+    Next Obligation. by destruct x0. Qed.
+    Next Obligation. by destruct x0. Qed.
+  End Triple_determines.
+
+  Section Bijection.
+    Program Definition bijective_1 (H' : C ⟶ Fun[2,B]) : H' ≡ H (S H') (T H') (τ H') := (_ ; _).
+    Next Obligation.
+      isomorphism.
+      - natural_transform; ss.
+        + destruct x0; exact id.
+        + pose proof (TwoHom_inv f). unfold TwoHom_inv_t in X.
+          by destruct x0, y; subst; ss.
+      - natural_transform; ss.
+        + destruct x0; exact id.
+        + pose proof (TwoHom_inv f). unfold TwoHom_inv_t in X.
+          by destruct x0, y; subst; ss.
+      - by destruct x0.
+      - by destruct x0.
+    Defined.
+    Next Obligation. by destruct x0. Qed.
+  
+    Program Definition bijective_2 (S' T' : C ⟶ B) (τ' : S' ⟹ T')
+      : ∃ S_iso : S' ≅[Fun[C,B]] S (H S' T' τ'),
+        ∃ T_iso : T' ≅[Fun[C,B]] T (H S' T' τ'),
+        τ' ≡ T_iso⁻¹ ∘ τ (H S' T' τ') ∘ S_iso := (_; (_; _)).
+    Next Obligation. by unshelve by isomorphism; try natural_transform. Defined.
+    Next Obligation. by unshelve by isomorphism; try natural_transform. Defined.
+  End Bijection.
+End Ex7. End Ex7.
 
 (* TODO : Ex8. *)
