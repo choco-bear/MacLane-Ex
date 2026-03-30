14f2375a269235f7ecdaaa4f62ac3c8a0614abba
choco-bear <kdy030529@gmail.com>
2026-03-31 00:40:57 +0900

    Solved Ex I.4.1

---
+++ exercises/Ch1/Section4.v
@@ -0,0 +1,16 @@
+Require Import Common.
+Require Sets Graphs Concrete.
+
+Module Ex1. Section Ex1.
+  Import Sets.
+  Context (S : Sets.Object).
+  
+  Program Definition fromS : Sets.t ⟶ Sets.t :=
+    {|
+      fobj := λ X,S → X;
+      fmap := λ X Y f h, f ∘ h
+    |}.
+
+  Program Definition eval : ((-×-)%sets ∘ ⟨ fromS , .↦ S ⟩) ⟹ id :=
+    {| component := λ X hs, hs.1 hs.2 |}.
+End Ex1. End Ex1.
