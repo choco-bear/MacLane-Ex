e2f4705ffbaa57ae8ef3f4a4c83c4fd6b0f58131
choco-bear <kdy030529@gmail.com>
2026-04-19 00:16:42 +0900

    Solved Ex II.3.1

---
+++ exercises/Ch2/Section3.v
@@ -0,0 +1,7 @@
+Require Import Common.
+
+Module Ex1.
+  (* Check BinaryProduct_preserves_IsMonoid. *)
+  (* Check BinaryProduct_preserves_IsGroupoid. *)
+  (* Check BinaryProduct_preserves_IsDiscrete. *)
+End Ex1.
