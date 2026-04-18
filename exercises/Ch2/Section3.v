e3dcc4db653260b58427200b94235bc678c2a425
choco-bear <kdy030529@gmail.com>
2026-04-19 00:33:47 +0900

    Solved Ex II.3.2

--- exercises/Ch2/Section3.v
+++ exercises/Ch2/Section3.v
@@ -5,3 +5,7 @@ Module Ex1.
   (* Check BinaryProduct_preserves_IsGroupoid. *)
   (* Check BinaryProduct_preserves_IsDiscrete. *)
 End Ex1.
+
+Module Ex2.
+  Check BinaryProduct_preserves_IsPreOrder.
+End Ex2.
