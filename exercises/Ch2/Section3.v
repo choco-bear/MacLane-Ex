2bd9c9f763fef0a8c9c27935629af01ac47d0210
choco-bear <kdy030529@gmail.com>
2026-04-19 00:36:22 +0900

    Add placeholders for Ex II.3.3-5 with TODO comments

--- exercises/Ch2/Section3.v
+++ exercises/Ch2/Section3.v
@@ -9,3 +9,15 @@ End Ex1.
 Module Ex2.
   (* Check BinaryProduct_preserves_IsPreOrder. *)
 End Ex2.
+
+Module Ex3.
+  (* TODO : Solve this after defining indexed product of categories. *)
+End Ex3.
+
+Module Ex4.
+  (* TODO : Solve this after defining Matr_K. *)
+End Ex4.
+
+Module Ex5.
+  (* TODO : Solve this after defining Top and Rng. *)
+End Ex5.
