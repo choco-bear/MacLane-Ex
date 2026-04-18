86b69798ecb3c8d8f0b6f78e8f06505aa8b9f8d6
choco-bear <kdy030529@gmail.com>
2026-04-19 06:33:23 +0900

    Solved Ex II.3.3

--- exercises/Ch2/Section3.v
+++ exercises/Ch2/Section3.v
@@ -15,9 +15,23 @@ Module Ex2.
   (* Check BinaryProduct.preserves_IsPreOrder. *)
 End Ex2.
 
-Module Ex3.
-  (* TODO : Solve this after defining indexed product of categories. *)
-End Ex3.
+Module Ex3. Section Ex3.
+  Import Product. Local Open Scope functor_scope.
+  Context `{A : Category ObjA} [I : Type] `{C : ∀ i : I, Category (ObjC i)}.
+
+  Lemma universal_property_existence i (F : ∀ i, A ⟶ C i) : Proj[C] i ∘ ∏ F = F i.
+  Proof. cby apply functor_ext. Qed.
+  
+  Lemma universal_property_uniqueness (F : ∀ i, A ⟶ C i) (G : A ⟶ ∏ C) (COMPAT : ∀ i, Proj[C] i ∘ G = F i) : G = ∏ F.
+  Proof.
+    apply functor_ext.
+    - apply func_ext=> a. apply func_ext_dep=> i.
+      specialize (COMPAT i). fapply fobj in COMPAT.
+      eapply equal_f in COMPAT. exact COMPAT.
+    - ii. eapply jmeq_func_ext_dep=> i. specialize (COMPAT i).
+      cut (JMeq ((Proj[C] i ∘ G) # f) (F i # f))%morphism; rewrite // ?COMPAT //.
+  Qed.
+End Ex3. End Ex3.
 
 Module Ex4.
   (* TODO : Solve this after defining Matr_K. *)
